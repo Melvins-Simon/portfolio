2e036338b402b4d34fce585b3bf3ab46c295397d
melvins simon <melvinssimon@gmail.com>
2025-05-10 12:43:35 +0300

    f:v3

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(
   cors({
     origin: [
       "https://melvins-simon-f2dqa4bcedcpefbq.eastus-01.azurewebsites.net",
-      "http://localhost:5173",
+      "http://localhost:5000",
     ],
     credentials: true,
   })
